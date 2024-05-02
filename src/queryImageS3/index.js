9fd02b53a2ee83b9a345b7a824dd26ddf5091b5e
Nattapon33998 <nattapon33998@gmail.com>
2024-05-02 18:40:10 +0700

    query image s3

---
+++ src/queryImageS3/index.js
@@ -0,0 +1,51 @@
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+
+const downloadImage = async (url, imagePath) => {
+  const writer = fs.createWriteStream(imagePath);
+
+  const response = await axios({
+    url,
+    method: "GET",
+    responseType: "stream",
+  });
+
+  response.data.pipe(writer);
+
+  return new Promise((resolve, reject) => {
+    writer.on("finish", resolve);
+    writer.on("error", reject);
+  });
+};
+
+async function main() {
+  const folderPath = "./src/queryImageS3/download/";
+
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath);
+  }
+
+  let projectData = await axios.get(
+    "https://developers.bitkubchain.com/api/v2/service/app-directory/page?page=1&limit=100"
+  );
+
+  for (i in projectData.data.data) {
+    const imageURL = projectData.data.data[i].image;
+    const imageName = path.basename(imageURL);
+    const imagePath = path.join(folderPath, imageName);
+    try {
+      await downloadImage(imageURL, imagePath);
+      console.log(`Image ${imageName} downloaded successfully!`);
+    } catch (error) {
+      console.error(`Error downloading image ${imageName}:`, error);
+    }
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
